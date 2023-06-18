3ab31b6b10033ee16538fdc5d7789a3d10971eaa
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-06-18 02:14:29 +0100

    make it clear Location is used

--- python/berlin/__init__.py
+++ python/berlin/__init__.py
@@ -1,3 +1,7 @@
-from berlin._berlin import load as load, load_from_json as load_from_json, Location as Location
+from berlin._berlin import (
+    load as load,
+    load_from_json as load_from_json,
+    Location as Location
+)
 
 __version__ = "0.3.7"
